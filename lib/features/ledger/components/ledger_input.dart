dfab57ca60909daadb061b2b482626288ec6c5f2
Marcus LEOW <leowmarcus13@hotmail.com>
2023-05-16 17:53:11 +0800

    added controller

--- lib/features/ledger/components/ledger_input.dart
+++ lib/features/ledger/components/ledger_input.dart
@@ -13,26 +13,38 @@ class LedgerInput extends StatefulWidget {
 class _LedgerInputState extends State<LedgerInput> {
   final TextEditingController _accountController = TextEditingController();
 
+  @override
+  void dispose() {
+    _accountController.dispose();
+    super.dispose();
+  }
+
   //Declare all state variables here, to be managed by this widget
-  TransactionType transactionType = TransactionType.expense;
+  TransactionType _transactionType = TransactionType.expense;
   void _changeType(Set<TransactionType> newType) {
     setState(() {
-      transactionType = newType.first;
+      _transactionType = newType.first;
     });
   }
+
+  String _account = '';
 
   ExpansionFormItem _buildData() {
     return ExpansionFormItem(
       formEntry: FormEntry(
+        //Information provided in this entry should be from the children widget
         leading: const Column(
           children: [Text('14'), Text('May'), Text('2023')],
         ),
-        title: const Text('Example Title'),
+        title: Text(_account),
         subtitle: const Text('example subtitle'),
         children: [
           //SegmentedButton for picking the type of transaction
           FormEntry(
-            title: TypePicker(type: transactionType, setType: _changeType),
+            title: TypePicker(
+              type: _transactionType,
+              setType: _changeType,
+            ),
           ),
           //Input for date
           //TODO refactor this to date picker
@@ -44,10 +56,16 @@ class _LedgerInputState extends State<LedgerInput> {
           //Input for account.s
           //TODO refactor this to a select widget
           FormEntry(
-              title: TextField(
-            controller: _accountController,
-            decoration: const InputDecoration(labelText: 'Account'),
-          )),
+            title: TextFormField(
+              controller: _accountController,
+              decoration: const InputDecoration(labelText: 'Account'),
+              onChanged: (value) {
+                setState(() {
+                  _account = value;
+                });
+              },
+            ),
+          ),
           const FormEntry(title: Text('Category')),
           const FormEntry(title: Text('Amount')),
           const FormEntry(title: Text('Additional Note')),
