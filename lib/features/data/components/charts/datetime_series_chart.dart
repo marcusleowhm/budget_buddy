8ad0da3836da32c14287c338336db7a085502051
Marcus LEOW <leowmarcus13@hotmail.com>
2023-06-12 23:38:58 +0800

    bug fix for setting y axis on chart

--- lib/features/data/components/charts/datetime_series_chart.dart
+++ lib/features/data/components/charts/datetime_series_chart.dart
@@ -1,9 +1,6 @@
-import 'dart:math';
-
 import 'package:budget_buddy/features/constants/enum.dart';
 import 'package:budget_buddy/features/ledger/cubit/c_transaction_cubit.dart';
 import 'package:budget_buddy/features/ledger/model/transaction_data.dart';
-import 'package:budget_buddy/utilities/chart_formatter.dart';
 import 'package:budget_buddy/utilities/currency_formatter.dart';
 import 'package:budget_buddy/utilities/date_formatter.dart';
 import 'package:fl_chart/fl_chart.dart';
@@ -30,32 +27,26 @@ class DateTimeSeriesChart extends StatefulWidget {
 class _DateTimeSeriesChartState extends State<DateTimeSeriesChart> {
   final double width = 12;
   int touchedGroupIndex = -1;
-  double maxY = 0.0;
-  double minY = 0.0;
-
-  // late double minY;
 
   List<BarChartGroupData> prepareBarGroups(BuildContext context) {
     List<TransactionData> data =
         context.read<CTransactionCubit>().state.committedEntries;
+
     List<BarChartGroupData> groups = [];
     DateTime now = DateTime.now().toLocal();
-    int currentMonth = now.month;
-    int currentYear = now.year;
-    double maxOfIncomeAndExpense = 0.0;
-    double minOfIncomeAndExpense = 0.0;
+    int currentLocalMonth = now.month;
+    int currentLocalYear = now.year;
 
     if (widget.dateFilter == ChartDateFilterCriteria.monthly) {
       //Starting month will always be 1, since Monthly filter implies Year to Date in Monthly intervals
-      for (int i = 1; i <= currentMonth; i++) {
+      for (int i = 1; i <= currentLocalMonth; i++) {
         double incomeSum = 0.0;
         double expenseSum = 0.0;
 
         // Calculate the sum of income and expense in a particular month
         for (TransactionData entry in data) {
           DateTime entryLocalDateTime = entry.utcDateTime.toLocal();
-          if (entryLocalDateTime.month == i &&
-              entryLocalDateTime.year == currentYear) {
+          if (entryLocalDateTime.month == i && entryLocalDateTime.year == currentLocalYear) {
             switch (entry.type) {
               case TransactionType.income:
                 incomeSum += entry.amount;
@@ -70,52 +61,20 @@ class _DateTimeSeriesChartState extends State<DateTimeSeriesChart> {
           }
         }
 
-        if (widget.amountFilter == ChartAmountDisplayCriteria.gross) {
-          if (maxOfIncomeAndExpense < max(incomeSum, expenseSum)) {
-            maxOfIncomeAndExpense = max(incomeSum, expenseSum);
-          }
-
-          //Round up to the nearest of the highest value in the time series
-          maxOfIncomeAndExpense = roundToNearestPositive(maxOfIncomeAndExpense);
-          setState(() {
-            maxY = maxOfIncomeAndExpense;
-            minY = 0.0;
-          });
-
-          //Add the group data to the list
-          groups.add(_makeDoubleBarGroupData(i, incomeSum, expenseSum));
-        }
-
-        if (widget.amountFilter == ChartAmountDisplayCriteria.nett) {
-          //If the income is higher than the expense, we can find the max Y
-          if (incomeSum > expenseSum &&
-              maxOfIncomeAndExpense < incomeSum - expenseSum) {
-            maxOfIncomeAndExpense = incomeSum - expenseSum;
-          }
-
-          //If the income is lower than the expense, we find the minimum Y
-          if (incomeSum < expenseSum &&
-              minOfIncomeAndExpense > incomeSum - expenseSum) {
-            minOfIncomeAndExpense = incomeSum - expenseSum;
-          }
-
-          //Round up to the nearest of the highest value in the time series
-          maxOfIncomeAndExpense = roundToNearestPositive(maxOfIncomeAndExpense);
-          minOfIncomeAndExpense = roundToNearestNegative(minOfIncomeAndExpense);
-          setState(() {
-            maxY = maxOfIncomeAndExpense;
-            minY = minOfIncomeAndExpense;
-          });
-
-          //Add the group data to the list
-          groups.add(_makeSingleBarGroupdata(i, incomeSum - expenseSum));
+        switch (widget.amountFilter) {
+          case ChartAmountDisplayCriteria.gross:
+            groups.add(_makeDoubleBarGroupData(i, incomeSum, expenseSum));
+            break;
+          case ChartAmountDisplayCriteria.nett:
+            groups.add(_makeSingleBarGroupdata(i, incomeSum - expenseSum));
+            break;
         }
       }
     }
 
     //Starting month will be the 5th past year from current year
     if (widget.dateFilter == ChartDateFilterCriteria.yearly) {
-      for (int i = currentYear - 4; i <= currentYear; i++) {
+      for (int i = currentLocalYear - 4; i <= currentLocalYear; i++) {
         double incomeSum = 0.0;
         double expenseSum = 0.0;
         for (TransactionData entry in data) {
@@ -135,46 +94,13 @@ class _DateTimeSeriesChartState extends State<DateTimeSeriesChart> {
           }
         }
 
-        //Calculated only if the data's year is equals to i
-        if (widget.amountFilter == ChartAmountDisplayCriteria.gross) {
-          if (maxOfIncomeAndExpense < max(incomeSum, expenseSum)) {
-            maxOfIncomeAndExpense = max(incomeSum, expenseSum);
-          }
-
-          //Round up to the next 50 of the highest value in the time series
-          maxOfIncomeAndExpense = roundToNearestPositive(maxOfIncomeAndExpense);
-          setState(() {
-            maxY = maxOfIncomeAndExpense;
-            minY = 0.0;
-          });
-
-          //Add the group data to the list
-          groups.add(_makeDoubleBarGroupData(i, incomeSum, expenseSum));
-        }
-
-        if (widget.amountFilter == ChartAmountDisplayCriteria.nett) {
-          //If the income is higher than the expense, we can find the max Y
-          if (incomeSum > expenseSum &&
-              maxOfIncomeAndExpense < incomeSum - expenseSum) {
-            maxOfIncomeAndExpense = incomeSum - expenseSum;
-          }
-
-          //If the income is lower than the expense, we find the minimum Y
-          if (incomeSum < expenseSum &&
-              minOfIncomeAndExpense > incomeSum - expenseSum) {
-            minOfIncomeAndExpense = incomeSum - expenseSum;
-          }
-
-          //Round up to the nearest of the highest value in the time series
-          maxOfIncomeAndExpense = roundToNearestPositive(maxOfIncomeAndExpense);
-          //Round down to the nearest of the lowest value in time series
-          minOfIncomeAndExpense = roundToNearestNegative(minOfIncomeAndExpense);
-          setState(() {
-            maxY = maxOfIncomeAndExpense;
-            minY = minOfIncomeAndExpense;
-          });
-          //Add the group data to the list
-          groups.add(_makeSingleBarGroupdata(i, incomeSum - expenseSum));
+        switch (widget.amountFilter) {
+          case ChartAmountDisplayCriteria.gross:
+            groups.add(_makeDoubleBarGroupData(i, incomeSum, expenseSum));
+            break;
+          case ChartAmountDisplayCriteria.nett:
+            groups.add(_makeSingleBarGroupdata(i, incomeSum - expenseSum));
+            break;
         }
       }
     }
@@ -217,6 +143,30 @@ class _DateTimeSeriesChartState extends State<DateTimeSeriesChart> {
         width: width,
       ),
     ]);
+  }
+
+  Widget _buildLeftTitles(double value, TitleMeta meta) {
+    double roundingThreshold = 0.5;
+
+    Widget axisTitle = Text(compactCurrencyFormatter.format(value));
+    // A workaround to hide the max value title as FLChart is overlapping it on top of previous
+    if (value == meta.max) {
+      final remainder = value % meta.appliedInterval;
+      if (remainder != 0.0 &&
+          remainder / meta.appliedInterval < roundingThreshold) {
+        axisTitle = const SizedBox.shrink();
+      }
+    }
+
+    if (value == meta.min) {
+      final remainder = -value % meta.appliedInterval;
+      if (remainder != 0.0 &&
+          -remainder / meta.appliedInterval < roundingThreshold) {
+        axisTitle = const SizedBox.shrink();
+      }
+    }
+
+    return SideTitleWidget(axisSide: meta.axisSide, child: axisTitle);
   }
 
   Widget _buildBottomTitles(double value, TitleMeta meta) {
@@ -281,23 +231,18 @@ class _DateTimeSeriesChartState extends State<DateTimeSeriesChart> {
               padding: const EdgeInsets.only(right: 20),
               child: BarChart(
                 BarChartData(
-                  maxY: (minY == 0 && maxY == 0.0) ? 50.0 : maxY,
-                  minY: minY == 0.0 ? 0.0 : minY,
-                  extraLinesData:
-                      widget.amountFilter == ChartAmountDisplayCriteria.nett
-                          ? ExtraLinesData(
-                              horizontalLines: [
-                                HorizontalLine(
-                                    y: 0,
-                                    strokeWidth: 0.5,
-                                    color: Colors.grey,
-                                    dashArray: [
-                                      20,
-                                      5,
-                                    ]),
-                              ],
-                            )
-                          : null,
+                  extraLinesData: ExtraLinesData(
+                    horizontalLines: [
+                      HorizontalLine(
+                          y: 0,
+                          strokeWidth: 0.5,
+                          color: Colors.grey,
+                          dashArray: [
+                            20,
+                            5,
+                          ]),
+                    ],
+                  ),
                   barTouchData: BarTouchData(
                     touchTooltipData: BarTouchTooltipData(
                       tooltipBgColor: Colors.grey[200],
@@ -361,7 +306,8 @@ class _DateTimeSeriesChartState extends State<DateTimeSeriesChart> {
                     leftTitles: AxisTitles(
                       sideTitles: SideTitles(
                         showTitles: true,
-                        reservedSize: 50,
+                        getTitlesWidget: _buildLeftTitles,
+                        reservedSize: 56,
                       ),
                     ),
                   ),
