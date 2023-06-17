216f2d29c344bb80ae4fc4d6212a73c49e9fcea0
Marcus LEOW <leowmarcus13@hotmail.com>
2023-06-17 15:45:59 +0800

    refactor piechart list

--- lib/features/data/components/statistics/piechart_breakdown/single_month_category_list.dart
+++ lib/features/data/components/statistics/piechart_breakdown/single_month_category_list.dart
@@ -68,13 +68,210 @@ class _SingleMonthCategoryListState extends State<SingleMonthCategoryList> {
     return total;
   }
 
+  Widget _buildFirstRow() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Click or Tap on amount to see full amount',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLastRow(double total) {
+    GlobalKey<TooltipState> totalTooltipKey = GlobalKey();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Expanded(
+            child: Text(
+              'Total',
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            children: [
+              //Sum
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                child: GestureDetector(
+                  onTap: () {
+                    totalTooltipKey.currentState?.ensureTooltipVisible();
+                  },
+                  child: Tooltip(
+                    message: englishDisplayCurrencyFormatter.format(total),
+                    triggerMode: TooltipTriggerMode.manual,
+                    showDuration: const Duration(seconds: 5),
+                    key: totalTooltipKey,
+                    child: Container(
+                      width: 90,
+                      padding: const EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        color: widget.type == TransactionType.income
+                            ? Colors.blue[900]
+                            : Colors.red[900],
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Text(
+                        total >= 1000 || total <= -1000
+                            ? compactCurrencyFormatter.format(total)
+                            : englishDisplayCurrencyFormatter.format(total),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Theme.of(context).canvasColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              //Percentage
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                child: Container(
+                  width: 60,
+                  padding: const EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    color: widget.type == TransactionType.income
+                        ? Colors.blue[900]
+                        : Colors.red[900],
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Text(
+                    '100%',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Theme.of(context).canvasColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  int colorValue = 900;
+  Widget _buildItemRow(
+      Map<String, double> categorySum, double total, int index) {
+    double percentage =
+        categorySum.entries.elementAt(index).value / total * 100;
+    GlobalKey<TooltipState> tooltipKey = GlobalKey();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //Category
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                print('TODO implement category trend feature');
+                //TODO implement
+              },
+              child: Text(
+                categorySum.entries.elementAt(index).key,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+
+          //Subtotal
+          Row(
+            children: [
+              //Sum
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                child: GestureDetector(
+                  onTap: () {
+                    tooltipKey.currentState?.ensureTooltipVisible();
+                  },
+                  child: Tooltip(
+                    message: englishDisplayCurrencyFormatter
+                        .format(categorySum.entries.elementAt(index).value),
+                    triggerMode: TooltipTriggerMode.manual,
+                    showDuration: const Duration(seconds: 5),
+                    key: tooltipKey,
+                    child: Container(
+                      width: 90,
+                      padding: const EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        color: widget.type == TransactionType.income
+                            ? Colors.blue[colorValue - 100 * index]
+                            : Colors.red[colorValue - 100 * index],
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Text(
+                        (categorySum.entries.elementAt(index).value >= 1000 ||
+                                categorySum.entries.elementAt(index).value <=
+                                    -1000)
+                            ? compactCurrencyFormatter.format(
+                                categorySum.entries.elementAt(index).value)
+                            : englishDisplayCurrencyFormatter.format(
+                                categorySum.entries.elementAt(index).value,
+                              ),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Theme.of(context).canvasColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              //Percentage
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                child: Container(
+                  width: 60,
+                  padding: const EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    color: widget.type == TransactionType.income
+                        ? Colors.blue[colorValue - 100 * index]
+                        : Colors.red[colorValue - 100 * index],
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Text(
+                    percentage > 0
+                        ? '${percentage.toStringAsFixed(0)}%'
+                        : 'N/A',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Theme.of(context).canvasColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CTransactionCubit, CTransactionState>(
       builder: (context, state) {
         Map<String, double> categorySum = filterByCriteria(state);
         double total = calculateTotal(categorySum);
-        int colorValue = 900;
 
         return categorySum.isEmpty
             ? const Text('No data added yet')
@@ -85,152 +282,41 @@ class _SingleMonthCategoryListState extends State<SingleMonthCategoryList> {
                     ListView.builder(
                       shrinkWrap: true,
                       physics: const NeverScrollableScrollPhysics(),
-                      itemCount: categorySum.length + 1,
+                      itemCount: categorySum.length,
                       itemBuilder: (context, index) {
-                        //Row header
-                        if (index == 0) {
-                          return const Padding(
-                            padding: EdgeInsets.symmetric(vertical: 10.0),
-                            child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    'Click or Tap on amount to see full amount',
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ]),
+                        //Row header for when there is only 1 item in the list
+                        if (index == 0 && categorySum.length == 1) {
+                          return Column(
+                            children: [
+                              _buildFirstRow(),
+                              _buildItemRow(categorySum, total, index),
+                              _buildLastRow(total),
+                            ],
                           );
                         }
 
-                        index--;
-                        double percentage =
-                            categorySum.entries.elementAt(index).value /
-                                total *
-                                100;
-                        GlobalKey<TooltipState> tooltipKey = GlobalKey();
-
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 4.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        //Row header for 2 or more lines of category
+                        if (index == 0) {
+                          return Column(
                             children: [
-                              //Category
-                              Expanded(
-                                child: GestureDetector(
-                                  onTap: () {
-                                    print(
-                                        'TODO implement category trend feature');
-                                    //TODO implement
-                                  },
-                                  child: Text(
-                                    categorySum.entries.elementAt(index).key,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                              ),
-
-                              //Subtotal
-                              Row(
-                                children: [
-                                  //Sum
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 5.0),
-                                    child: GestureDetector(
-                                      onTap: () {
-                                        tooltipKey.currentState
-                                            ?.ensureTooltipVisible();
-                                      },
-                                      child: Tooltip(
-                                        message: englishDisplayCurrencyFormatter
-                                            .format(categorySum.entries
-                                                .elementAt(index)
-                                                .value),
-                                        triggerMode: TooltipTriggerMode.manual,
-                                        showDuration:
-                                            const Duration(seconds: 5),
-                                        key: tooltipKey,
-                                        child: Container(
-                                          width: 90,
-                                          padding: const EdgeInsets.all(5.0),
-                                          decoration: BoxDecoration(
-                                            color: widget.type ==
-                                                    TransactionType.income
-                                                ? Colors.blue[
-                                                    colorValue - 100 * index]
-                                                : Colors.red[
-                                                    colorValue - 100 * index],
-                                            borderRadius:
-                                                BorderRadius.circular(10.0),
-                                          ),
-                                          child: Text(
-                                            (categorySum.entries
-                                                            .elementAt(index)
-                                                            .value >
-                                                        1000 ||
-                                                    categorySum.entries
-                                                            .elementAt(index)
-                                                            .value <
-                                                        -1000)
-                                                ? compactCurrencyFormatter
-                                                    .format(categorySum.entries
-                                                        .elementAt(index)
-                                                        .value)
-                                                : englishDisplayCurrencyFormatter
-                                                    .format(
-                                                    categorySum.entries
-                                                        .elementAt(index)
-                                                        .value,
-                                                  ),
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(
-                                              color:
-                                                  Theme.of(context).canvasColor,
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  //Percentage
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 5.0),
-                                    child: Container(
-                                      width: 60,
-                                      padding: const EdgeInsets.all(5.0),
-                                      decoration: BoxDecoration(
-                                        color: widget.type ==
-                                                TransactionType.income
-                                            ? Colors
-                                                .blue[colorValue - 100 * index]
-                                            : Colors
-                                                .red[colorValue - 100 * index],
-                                        borderRadius:
-                                            BorderRadius.circular(10.0),
-                                      ),
-                                      child: Text(
-                                        percentage > 0
-                                            ? '${percentage.toStringAsFixed(0)}%'
-                                            : 'N/A',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                          color: Theme.of(context).canvasColor,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
+                              _buildFirstRow(),
+                              _buildItemRow(categorySum, total, index),
                             ],
-                          ),
-                        );
+                          );
+                        }
+
+                        //Last Row
+                        if (index == categorySum.length - 1) {
+                          return Column(
+                            children: [
+                              _buildItemRow(categorySum, total, index),
+                              _buildLastRow(total)
+                            ],
+                          );
+                        }
+
+                        //Row in betweeen
+                        return _buildItemRow(categorySum, total, index);
                       },
                     ),
                   ],
